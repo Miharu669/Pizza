d9d65c4378883fcfaed3ec26861a737f79970f10
Miharu669 <veronicadoelfuentes@gmail.com>
2024-05-24 09:56:06 +0200

    main

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
 
 const ingredients = ["flour","water","pinchSalt","beer","yeast","aove","redPepper","mince","springOnion","barbequeSauce","tomatoSauce"]
-const OvenIsRdy = async() => { return new Promise (resolve => serTimeout (() => {
-     resolve ('Beep! Oven is ready')}
-     , 2500 ));
-}
 
-const preHeat = async ()=> {
+const preHeat = async () => {
     console.log ('PreHeated');
     const response = await OvenIsRdy()
     console.log(response)
 }
 
-function makeDough(o) {
-    let dough 
-    let result = []
-    for (
-        dough = 0;
-        dough !==null;
-        dough =
-        dough.getPrototypeOf(dough))
-        {
-           result = result.concat(flour, water, beer, pinchSalt, yeast, aove) ;
-        }
-        return result;
-        }
+
+const OvenIsRdy = async() => { return new Promise (resolve => setTimeout (() => {
+     resolve ('Beep! Oven is ready')}
+     , 2500 ));
+}
+
+function app() {
+    preHeat()
+    makePizza()
+}
+
+app()
+
+
+function makePizza() {
+    makeDough()
+}
+
+function makeDough() {
+    console.log("Preparar la masa");
+}
     
-    /*let redPepper = 1
+    /* let redPepper = 1
     let mince = 500
     let springOnion = 1
     let barbequeSauce = 1
